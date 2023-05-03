e6e8ca6e32ee86718b978b389beb8e9876da9848
hkhs35 <131329311+hkhs35@users.noreply.github.com>
2023-05-04 00:42:50 +0800

    更正

--- main.ts
+++ main.ts
@@ -1,45 +1,47 @@
 input.onButtonPressed(Button.A, function () {
-    fruit.change(LedSpriteProperty.X, -1)
+    monkey.change(LedSpriteProperty.X, -1)
+})
+input.onGesture(Gesture.Shake, function () {
+    music.playMelody("C5 B A G G A B C5 ", 180)
 })
 input.onButtonPressed(Button.AB, function () {
     game.setScore(0)
-    sprite = 1
+    run = 1
     time = 500
 })
 input.onButtonPressed(Button.B, function () {
-    fruit.change(LedSpriteProperty.X, 1)
+    monkey.change(LedSpriteProperty.X, 1)
 })
-let _this: game.LedSprite = null
 let banana: game.LedSprite = null
 let time = 0
-let sprite = 0
-let fruit: game.LedSprite = null
-fruit = game.createSprite(2, 4)
 let run = 0
+let monkey: game.LedSprite = null
+monkey = game.createSprite(2, 4)
+run = 0
 basic.forever(function () {
     if (run == 1) {
         banana = game.createSprite(randint(0, 4), 0)
         banana.set(LedSpriteProperty.Brightness, 50)
         for (let index = 0; index <= 4; index++) {
             banana.set(LedSpriteProperty.Y, index)
+            basic.pause(time)
         }
-        basic.pause(time)
+        banana.delete()
+        time += -20
     }
-    banana.delete()
-    _this += -20
 })
 basic.forever(function () {
     if (run == 1) {
-        if (fruit.isTouching(banana)) {
+        if (monkey.isTouching(banana)) {
             game.addScore(1)
             banana.delete()
         }
     }
 })
 basic.forever(function () {
-    if (true) {
-        _this = game.createSprite(randint(0, 4), 2)
-        fruit.set(LedSpriteProperty.Brightness, 50)
+    if (run == 1) {
+        banana = game.createSprite(randint(0, 4), 0)
+        banana.set(LedSpriteProperty.Brightness, 50)
         for (let index = 0; index <= 5; index++) {
             if (banana.isDeleted()) {
                 break;
@@ -49,9 +51,11 @@ if (index == 5) {
                     game.pause()
                     basic.showNumber(game.score())
                 }
+                banana.set(LedSpriteProperty.Y, index)
             }
+            basic.pause(time)
         }
         banana.delete()
-        time = -20
+        time += -20
     }
 })
